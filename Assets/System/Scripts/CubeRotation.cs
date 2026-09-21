ff617098a7b1e85f91f79ec50573ff5d8fa3e384
C-DGameMaker <charlotte5634@outlook.com>
2026-09-21 17:15:18 -0300

    Rebuilt the game

--- Assets/System/Scripts/CubeRotation.cs
+++ Assets/System/Scripts/CubeRotation.cs
@@ -1,20 +1,28 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+/// <summary>
+/// Makes it so the cube can Rotate 
+/// </summary>
 public class CubeRotation : MonoBehaviour
 {
+    // A bool is used to determine when the cube can spin
     [SerializeField] bool canSpin;
 
+    // In start, we set the bool to false as we don't want it spinning automatically
     private void Start()
     {
         canSpin = false;
     }
 
+    // This uses the Event system. This will trigger only when the targetted button is hit
+    // This causes the bool to change to whatever it is not. 
     public void CubeSpinning(InputAction.CallbackContext context)
     {
         canSpin = !canSpin;
     }
 
+    // The update checks if the cube can spin, and if the bool is true
+    // It rotates the cube.
     public void Update()
     {
         if (canSpin == true)
--- Assets/System/Scripts/TextChanging.cs
+++ Assets/System/Scripts/TextChanging.cs
@@ -1,22 +1,34 @@
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
-
+/// <summary>
+/// This is used to determine whether or not to change the text
+/// and what it should say at the bottom of the screen
+/// </summary>
 public class TextChanging : MonoBehaviour
 {
+    // This is used to find and change the text of the text object we want 
+    // the text to change on
     public TextMeshProUGUI textToChange;
 
+    // A bool is used to determine when the cube can spin
     [SerializeField] bool canSpin;
 
+    // In start, we set the bool to false as we don't want it spinning automatically
     private void Start()
     {
         canSpin = false;
     }
 
+    // This uses the Event system. This will trigger only when the targetted button is hit
     public void ChangeText(InputAction.CallbackContext context)
     {
+        // First, it sets the canSpin bool to whatever the opposite of it was
         canSpin = !canSpin;
 
+        // Then it checks the bool current property.
+        // If it's true, it switches the text to say "The Cube is spinning"
+        // If false, it switches the text to say "The Cube is not spinning"
         if (canSpin == true)
         {
             textToChange.text = "The Cube is spinning";
